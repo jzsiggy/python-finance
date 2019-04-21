d8c1c56224a4f513cb9c34decd3e0e2d7f730ec4
Joao Zsigmond <jzsiggy@gmail.com>
2019-04-21 14:12:51 -0300

    compiling snp500 data to one df

--- SnP500.py
+++ SnP500.py
@@ -46,3 +46,32 @@ def get_data(reload_sp500=False):
             print('Already have {}'.format(ticker))
 
 #  get_data()
+
+def compile_data():
+    with open('sp500tickers.pickle', 'rb') as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        if os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+            df.set_index('Date', inplace=True)
+
+            df.rename(columns = {'Adj Close': ticker}, inplace=True)
+            df.drop(['Open','High','Low','Close','Volume'], 1, inplace=True)
+
+            if main_df.empty:
+                main_df = df
+            else:
+                main_df = main_df.join(df, how='outer')
+        else:
+            print('No data available for {}'.format(ticker))
+
+    print(main_df.head())
+    print(main_df.tail())
+    main_df.to_csv('SP500_joined_closes.csv')
+
+compile_data()
+        
+    
