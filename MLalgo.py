62116b8fdcd3535f96a7886a0ef8d1dcd6aaa725
Joao Zsigmond <jzsiggy@gmail.com>
2019-04-22 15:55:44 -0300

    buy_sell_hold

--- MLalgo.py
+++ MLalgo.py
@@ -1,6 +1,8 @@
+from collections import Counter
 import numpy as np 
 import pandas as pd 
 import pickle
+# import SnP500
 
 def process_data_for_labels(ticker):
     hm_days = 7 # how many days to account for
@@ -15,5 +17,46 @@ def process_data_for_labels(ticker):
     # print(df.head())
     return tickers, df
 
-process_data_for_labels('AAP')
+# process_data_for_labels('AAP')
 
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    requirement = 0.02
+    for col in cols:
+        if col>requirement:
+            return 1
+        if col< -requirement:
+            return -1
+    return 0
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+
+    df['{}_target'.format(ticker)] = list(map( buy_sell_hold, 
+                                                df['{}_1d'.format(ticker)],
+                                                df['{}_2d'.format(ticker)],
+                                                df['{}_3d'.format(ticker)],
+                                                df['{}_4d'.format(ticker)],
+                                                df['{}_5d'.format(ticker)],
+                                                df['{}_6d'.format(ticker)],
+                                                df['{}_7d'.format(ticker)]
+                                                ))
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread: ', Counter(str_vals))
+
+    df.fillna(0, inplace=True)
+
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)
+
+    X = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+
+    return X, y, df
+
+extract_featuresets('AAPL')
